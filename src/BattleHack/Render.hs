40eacd612f7a150b9c975a42952af60937f6f0c1
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-09-12 20:39:50 +0200

    Plenty of new modules. Mostly boilerplate

---
+++ src/BattleHack/Render.hs
@@ -0,0 +1,129 @@
+-- |
+-- Module      : BattleHack.Render
+-- Description :
+-- Copyright   : (c) Jonatan H Sundqvist, 2015
+-- License     : MIT
+-- Maintainer  : Jonatan H Sundqvist
+-- Stability   : experimental
+-- Portability : POSIX
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- GHC pragmas
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- API
+--------------------------------------------------------------------------------------------------------------------------------------------
+module BattleHack.Render where
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- We'll need these
+--------------------------------------------------------------------------------------------------------------------------------------------
+import Control.Monad (forM_)
+import Data.Complex
+-- import qualified Data.Set as S
+
+import qualified Graphics.Rendering.Cairo as Cairo
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- Types
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- |
+data KeyLayout = KeyLeft | KeyRight | KeyBoth | KeyAccidental
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- Data
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- |
+naturals :: Integral n => [n]
+naturals = [0, 2, 4, 5, 7, 9, 11]
+
+
+-- |
+accidentals :: Integral n => [n]
+accidentals = [1, 3, 6, 8, 10]
+
+
+-- |
+layout :: RealFloat r => Complex r -> r -> r -> KeyLayout -> [Complex r]
+layout (sx:+sy) indent mid KeyRight      = [0:+0,           (sx-indent):+0,   (sx-indent):+mid, sx:+mid, sx:+sy,  0:+sy]                  -- Right indent
+layout (sx:+sy) indent mid KeyBoth       = [indent:+0,      (sx-indent):+0,   (sx-indent):+mid, sx:+mid, sx:+sy,  0:+sy,      0:+mid, indent:+mid ]                  -- Double indent
+layout (sx:+sy) indent mid KeyLeft       = [indent:+0,      sx:+0,            sx:+sy,           0:+sy,   0:+mid,  indent:+mid] -- Left indent
+layout (sx:+sy) indent mid KeyAccidental = [(sx-indent):+0, (sx-indent):+mid, (sx+indent):+mid, (sx+indent):+0]                           -- Accidental
+  where
+    nw = 0:+0   -- North west
+    ne = sx:+0  -- North east
+    se = sx:+sy -- South east
+    sw = 0:+sy  -- South west
+
+    le = indent:+mid      -- Left indent
+    re = (sx-indent):+mid -- Right indent
+
+    lm = 0:+mid  -- Left middle
+    rm = sx:+mid -- Right middle
+
+
+
+-- |
+chordlayout :: [KeyLayout]
+chordlayout = [KeyRight, KeyAccidental, KeyBoth, KeyAccidental, KeyLeft, KeyRight, KeyAccidental, KeyBoth, KeyAccidental, KeyBoth, KeyAccidental, KeyLeft]
+
+
+-- | Horizontal offset for each key in the octave, relative to the very first key
+keysteps :: RealFloat r => [r]
+keysteps = scanl1 (+) [0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- Functions
+--------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------
+-- |
+vectorise :: RealFloat f => (f -> f -> a) -> Complex f -> a
+vectorise f (x:+y) = f x y
+
+
+-- |
+-- |
+polygon :: [Complex Double] -> Cairo.Render ()
+polygon (p:oints) = vectorise Cairo.moveTo p >> forM_ (oints++[p]) (vectorise Cairo.lineTo)
+
+
+-- |
+key :: (Complex Double, Double, Double) -> Complex Double -> Int -> Cairo.Render ()
+key (size, indent, mid) origin i = do
+  polygon . map (+origin) $ keylayout
+  where
+    keylayout     = layout size indent mid (chordlayout !! (i `mod` 12)) :: [Complex Double]
+
+
+-- |
+claviature :: Cairo.Render ()
+claviature = do
+  forM_ (zip keysteps [0..11]) $ \(offx, ikey) -> do
+    key (size, indent, mid) ((ox+sx*offx):+oy) ikey
+    if ikey `elem` naturals
+      then Cairo.setSourceRGBA 0.4 (1/7 * fromIntegral (ikey `mod` 7)) 0.75 1.0
+      else Cairo.setSourceRGBA 0.0 0.0                                 0.00 1.0
+    Cairo.fill
+
+    -- key ((ox+sx*fromIntegral ikey):+oy) ikey
+    -- Cairo.setSourceRGBA 0.0 0.0 0.0 1.0
+    -- Cairo.stroke
+    return ()
+  where
+    origin@(ox:+oy) = 150:+150
+    size@(sx:+sy)   = 40:+130
+    indent          = sx*0.26
+    mid             = sy*0.62
--- src/BattleHack/Types.hs
+++ src/BattleHack/Types.hs
@@ -1,11 +1,13 @@
 -- |
--- Module      : Main
--- Description : Entry point module
+-- Module      : BattleHack.Types
+-- Description :
 -- Copyright   : (c) Jonatan H Sundqvist, 2015
 -- License     : MIT
--- Maintainer  : sample@email.com
+-- Maintainer  : Jonatan H Sundqvist
 -- Stability   : experimental
 -- Portability : POSIX
+
+-- Created September 12 2015
 
 
 
@@ -18,57 +20,18 @@
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- API
 --------------------------------------------------------------------------------------------------------------------------------------------
-module Main where
+module BattleHack.Types where
 
 
 
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- We'll need these
 --------------------------------------------------------------------------------------------------------------------------------------------
-import Control.Monad      (forM_)
-import Control.Concurrent (threadDelay)
-import Text.Printf
 
-import qualified Graphics.Rendering.Cairo as Cairo
-import           Graphics.UI.Gtk          as Gtk
-import           Graphics.UI.Gtk          (AttrOp(..), on)
-
-import qualified BattleHack.Render as Render
 
 
 
 --------------------------------------------------------------------------------------------------------------------------------------------
--- Entry point
+-- Data
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- |
-main :: IO ()
-main = do
-  Gtk.initGUI
-  window <- Gtk.windowNew
-  frame  <- Gtk.frameNew
-
-  -- Gtk.windowTitle := "Chordially"
-  Gtk.set window [Gtk.windowTitle := "Chordially"]
-
-  canvas <- Gtk.drawingAreaNew
-  containerAdd frame canvas
-  Gtk.set window [ Gtk.containerChild := frame ]
-  Gtk.windowSetDefaultSize window 720 480
-
-  widgetAddEvents canvas [PointerMotionMask] -- MouseButton1Mask
-  Gtk.widgetShowAll window
-
-  --
-  window `on` Gtk.deleteEvent $ (Cairo.liftIO $ Gtk.mainQuit >> return False)
-  canvas `on` Gtk.draw        $ (Render.claviature)
-
-  Gtk.mainGUI
-
-
--- |
-goodbyeWorld :: IO ()
-goodbyeWorld = do
-  putStrLn "Hello world!"
-  putStrLn "Counting down to launch..."
-  forM_ [9,8..0] ((>> threadDelay (10^6)) . print)
-  putStrLn "Launching. What have I done!?"
